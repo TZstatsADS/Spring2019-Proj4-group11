a34c1bbafaad06755f23d80eedaabc8b90d8c8e2
Yujie Wang <yujiewang1110@gmail.com>
2019-04-16 21:21:40 -0400

    Create perform_correction.R

---
+++ lib/perform_correction.R
@@ -0,0 +1,14 @@
+perform_correction <- function(typo){
+    # check if typo has any candiates
+    candidates <- choose_candidate(typo)
+    if(length(candidates)==0) {
+      return(typo)
+    }
+    else {
+      #choose the candidate with highest score
+      scores <- sapply(candidates, calculate_score, typo) 
+      candidate_chosen <- candidates[which.max(scores)]
+      return(candidate_chosen)
+    }
+}
+
